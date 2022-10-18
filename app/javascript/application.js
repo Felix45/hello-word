e10ee55dbc4c592d51c925b45575ecc94f1dd2d4
Felix Ouma <fatonoh@gmail.com>
2022-10-18 23:47:44 +0300

    Add react root element to the webpage

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import React from "react"
+import ReactDOM from "react"
+
+const App = () => <h1>Hello, World</h1>
+
+ReactDOM.render(<App/>, document.getElementById('root'))
