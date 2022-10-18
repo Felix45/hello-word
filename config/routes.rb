b45cc20c8ac70cbc7d98c37257474d4247e69ef7
Felix Ouma <fatonoh@gmail.com>
2022-10-19 01:57:09 +0300

    Add api routes to routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  namespace :api do
+    namespace :v1 do
+      resources :greetings
+    end
+  end
+
   root "root#index"
 end
