57dbac449bfadcd9802236ee9133559ddb83193a
Felix Ouma <fatonoh@gmail.com>
2022-10-19 04:14:57 +0300

    Add Router, Provider and store object to the application

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
+import Greeting from "./controllers/components/Greeting";
 
-const App = () => <h1>Hello, World</h1>
+const App = () => <Greeting />
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+ReactDOM.render(
+       <Provider store={store}>
+         <BrowserRouter>
+           <App />
+         </BrowserRouter>
+       </Provider>, document.getElementById('root'))
