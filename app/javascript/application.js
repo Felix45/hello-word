60ecb5f82e8af4371a845c44f26bd13023e57e0b
Felix Ouma <fatonoh@gmail.com>
2022-10-19 04:50:07 +0300

    Add greetings component as a react route

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,15 +1,28 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { NavLink } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from "react-redux";
 import store from "./redux/configureStore";
 import Greeting from "./controllers/components/Greeting";
 
-const App = () => <Greeting />
+const App = () => {
+  return (
+    <Routes>
+      <Route exact path="/greet" element={<Greeting />} />
+    </Routes>
+  )
+}
 
 ReactDOM.render(
        <Provider store={store}>
          <BrowserRouter>
+           <NavLink to="/" role="button">
+              <span>Home | </span>
+           </NavLink>
+           <NavLink to="/greet" role="button">
+              <span>Greet someone</span>
+            </NavLink>
            <App />
          </BrowserRouter>
        </Provider>, document.getElementById('root'))
